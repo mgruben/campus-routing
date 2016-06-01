7e354bd3d4d47919d7e38c387614f31dc9c8d4eb
mgruben <GrubenM@gmail.com>
2016-06-01 13:53:26 -0500

    Fix WeightedEdge definition
    Although conceptually correct, having WeightedEdge and TwoWeightedEdge class definitions for this
    problem set is likely to lead to problems, as the automated grader presumes that what I previously
    called TwoWeightedEdge is actually called WeightedEdge, and that what I previously called WeightedEdge
    is not defined for this problem set.
    
    Accordingly, delete WeightedEdge, and rename TwoWeightedEdge to WeightedEdge.

--- graph.py
+++ graph.py
@@ -32,17 +32,9 @@ class Edge(object):
         return self.dest
     def __str__(self):
         return '{0}->{1}'.format(self.src, self.dest)
-        
-class WeightedEdge(Edge):
-    def __init__(self, src, dest, weight = 1.0):
-        Edge.__init__(src, dest)
-        self.weight = weight
-    def getWeight(self):
-        return self.weight
-    def __str__(self):
-        return str(self.src) + '->' + str(self.dest) +' (' + str(self.weight) + ')'
 
-class TwoWeightedEdge(Edge):
+
+class WeightedEdge(Edge):
     def __init__(self, src, dest, weight1 = 1.0, weight2 = 1.0, wname1 = "weight1", wname2 = "weight2"):
         Edge.__init__(src, dest)
         self.weight1 = weight1
